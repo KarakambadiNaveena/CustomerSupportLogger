018531f7a2d61f4216e95a3aafe8d9d08be52560
KarakambadiNaveena <naveenakarakambadi@gmail.com>
2022-11-02 16:28:50 +0530

    Add project files.

---
+++ DAL/operations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace DAL
+{
+    public class operations
+    {
+        customerdbEntities context = new customerdbEntities();
+        userinfo c = new userinfo();
+
+        public bool ValidateUser(string email, string password)
+        {
+
+            bool result = false;
+
+
+            if (c.email == "abc@123" && c.password == "abc")
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+        public void insert( custloginfo c)
+         {
+             context.custloginfoes.Add(c);
+             context.SaveChanges();
+         }
+        
+       
+
+
+        }
+
+    }
+
+    
+
